4b8c5e1c12697eab3c6c0bb0e68da909c1db5748
Dhiraj455 <dhirajshelke16@gmail.com>
2024-03-18 00:15:39 +0530

    Get Session API

--- backend/controllers/food.js
+++ backend/controllers/food.js
@@ -1,5 +1,5 @@
 const Food = require("../models/Food");
-const formData = require("form-data")
+const formData = require("form-data");
 const axios = require("axios");
 
 module.exports.addFood = async (req, res) => {
@@ -11,7 +11,7 @@ module.exports.addFood = async (req, res) => {
   try {
     console.log(req.body, "Body");
     console.log(req.file, "Files");
-    const {name,calories,protein,carbs,fats,fiber} = req.body;
+    const { name, calories, protein, carbs, fats, fiber } = req.body;
     const todayDate = new Date();
     const sessionDay = todayDate.toDateString();
     let food;
@@ -63,6 +63,28 @@ module.exports.addFood = async (req, res) => {
     }
     response.success = true;
     response.message = "Food added successfully";
+    return res.status(200).json(response);
+  } catch (err) {
+    console.log(err);
+    response.errMessage = err.message;
+    res.status(500).json(response);
+  }
+};
+
+module.exports.getAllSession = async (req, res) => {
+  let response = { success: false, message: "", data: [], errMessage: "" };
+  try{
+    const session = await Food.Session.find({ createdBy: "65f2e685ee2ccd52aea97cd6" });
+    if (session.length === 0) {
+      response.message = "No session found";
+      return res.status(200).json(response);
+    }
+    for (let i = 0; i < session.length; i++) {
+      const food = await Food.Food.find({ sessionId: session[i]._id });
+      response.data.push({ session: session[i], food });
+    }
+    response.success = true;
+    response.message = "Session found successfully";
     return res.status(200).json(response);
   } catch (err) {
     console.log(err);
